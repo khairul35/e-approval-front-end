a62f6023785609a48c4c730f1afba44936cc7f93
Khairul Habib Bin Samaon <57700102+khairul35@users.noreply.github.com>
2023-07-08 07:55:10 +0800

    change endpoint

--- src/repository/Repository.ts
+++ src/repository/Repository.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const repository = axios.create({
-  // baseURL: 'https://e-approval-backend.as.r.appspot.com/api/',
-  baseURL: 'http://localhost:3000/api/',
+  baseURL: 'https://e-approval-backend.as.r.appspot.com/api/',
+  // baseURL: 'http://localhost:3000/api/',
   headers: {
     'Authorization': 'Bearer ' + localStorage.getItem('accessToken'),
     'Content-Type': 'application/json',
